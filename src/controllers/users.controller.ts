a64ab471c3b2ff78812caadd9c23b3256a200463
Taylor Asprilla <54963612+TaylorAsprilla@users.noreply.github.com>
2024-09-19 20:30:56 -0500

    CRUD
    Co-authored-by: aurik3 <aurik3@users.noreply.github.com>

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -2,11 +2,72 @@ import { Request, Response } from "express";
 import { UserModel } from "../models/user.model";
 
 export const getUsers = async (req: Request, res: Response) => {
-  res.json({
-    ok: true,
-    msg: "Request Exitosoioioioooiio",
-  });
+  try {
+    const getAllUsers = await UserModel.find({});
+
+    res.json({ ok: true, users: getAllUsers });
+  } catch (error) {
+    res.status(500).json({ ok: false, msg: "Error occurred", error });
+  }
 };
+
+export const getUserByDocumentoNumber = async (req: Request, res: Response) => {
+  const documentNumber = req.params.document;
+
+  try {
+    const getUserByDocumentNumber = await UserModel.findOne({ documentNumber });
+
+    res.json({ ok: true, user: getUserByDocumentNumber });
+  } catch (error) {
+    res.status(500).json({ ok: false, msg: "Error occurred", error });
+  }
+};
+
+export const getUserById = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  try {
+    const getUserById = await UserModel.findById(id);
+    res.json({ ok: true, user: getUserById });
+  } catch (error) {
+    res.status(500).json({ ok: false, msg: "Error occurred", error });
+  }
+};
+
+export const updateUserById = async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const payload = req.body;
+
+  try {
+    const updateUserById = await UserModel.findByIdAndUpdate(id, payload, {
+      new: true,
+    });
+
+    if (updateUserById) {
+      res.json({ ok: true, user: updateUserById });
+    } else {
+      res.status(500).json({ ok: false, msg: "Error occurred to update user" });
+    }
+    
+  } catch (error) {
+    res.status(500).json({ ok: false, msg: "Error occurred", error });
+  }
+};
+
+export const deleteUserById = async (req: Request, res: Response) => {
+  const id = req.params.id;
+  try {
+    const deleteUser = await UserModel.findByIdAndDelete(id);
+    if(deleteUser) {
+      res.json({ ok: true, msg: "User deleted" });
+    }else{
+      res.status(500).json({ ok: false, msg: "Error occurred to delete user" });
+    }
+   
+  } catch (error) {
+    res.status(500).json({ ok: false, msg: "Error occurred", error });
+  }
+}
 
 export const createUsers = async (req: Request, res: Response) => {
   try {
@@ -26,8 +87,9 @@ export const createUsers = async (req: Request, res: Response) => {
   } catch (error) {
     console.error(error);
     res.status(500).json({
-        ok: false,
-        msg: "Error occurred"
-    })
+      ok: false,
+      msg: "Error occurred",
+      error,
+    });
   }
 };
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
-import { createUsers, getUsers } from "../controllers/users.controller";
+import { createUsers, getUserByDocumentoNumber, getUsers , getUserById, updateUserById, deleteUserById} from "../controllers/users.controller";
 
 const router = Router();
 
 router.get("/", getUsers);
+router.get('/document/:document', getUserByDocumentoNumber)
+router.get('/id/:id', getUserById)
 router.post("/", createUsers);
+router.put('/update/:id',updateUserById)
+router.delete('/delete/:id',deleteUserById)
+
 
 export default router
